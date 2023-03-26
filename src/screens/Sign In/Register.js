bd9376c2d8dfc25baa95cd5af7a59671cbc6e8d2
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-03-26 23:02:41 +0200

    tdee fix

--- src/screens/Sign In/Register.js
+++ src/screens/Sign In/Register.js
@@ -22,7 +22,7 @@ const Register = () => {
                 <input type="checkbox" id="terms" className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 mr-2' required=""/>
                 <label for="terms" className='font-light text-gray-500'>I accept the Terms and Conditions</label>
             </div>
-            <button type='submit' className='bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'>Create an account</button>
+            <button type='submit' className='bg-blue-700 hover:bg-blue-600  font-medium rounded-lg text-sm px-5 py-2.5 text-center'>Create an account</button>
             <Link to="/login"><p class="text-sm font-light text-gray-500">
                       Already have an account? <Link to="/login" className="font-medium text-primary-600 hover:underline">Login here</Link>
                   </p></Link>
