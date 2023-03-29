10376a10e20de844d1c879408a88df66eb70616f
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-03-29 20:42:45 +0200

    bodyweight page

--- src/screens/Calculators/Bodyweight.js
+++ src/screens/Calculators/Bodyweight.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Bodyweight = () => {
+
+  const [age, setAge] = useState()
+  const [height, setHeight] = useState()
+  let result
+
+  const handleSubmit = () => {
+
+  }
   return (
-    <div>Bodyweight</div>
+    <div className=' container mx-auto'>
+      <div className='flex flex-col gap-4 mt-7 text-white '>
+        <h1 className='text-xl'>Calculate your ideal bodyweight for your height</h1>
+        <p className='text-lg'>Maintaning ideal bodyweight is beneficial for your long term health</p>
+      </div>
+      <div className=' w-3/4 mx-auto bg-gray-700 py-5 px-4 my-4 rounded-lg md:w-1/2'>
+        <form>
+          <div className='flex flex-col gap-4'>
+            <input className='bg-gray-800 w-1/2 rounded-md py-1 px-2 text-sm focus:ring-2 focus:outline-none focus:ring-primary-300 md:w-1/2 md:text-lg text-white' type="number" />
+            <input className='bg-gray-800 w-1/2 rounded-md py-1 px-2 text-sm focus:ring-2 focus:outline-none focus:ring-primary-300 md:w-1/2 md:text-lg' type="number" />
+          </div>
+          <button type='submit' className='text-white bg-red-800 py-2 px-3  hover:bg-red-700 rounded-lg mt-5' >Calculate</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
