a26d9beacdc9619aa60ea30d2786d7ab9f9a89fc
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-03-30 23:13:19 +0200

    1rm calc

--- src/screens/Calculators/Calculator.js
+++ src/screens/Calculators/Calculator.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Calculators.css";
 
 const Calculator = () => {
   const [lift, setLift] = useState();
@@ -49,76 +50,113 @@ const Calculator = () => {
     event.preventDefault();
     calculate();
     clear();
-    console.log("WERKS!!")
   };
 
   return (
-    <div className="container mx-auto my-8">
-      <div className="border py-7 px-5 rounded-xl">
-        <div className="text-white my-10">
-          <h1 style={{ fontWeight: "600" }} className="text-2xl mb-4">
+    <div className="container mx-auto my-8 grid grid-cols-1 gap-7 md:grid-cols-2">
+      <div className=" py-7 px-5 rounded-xl md:border">
+        <div className="text-white mt-5 mb-14">
+          <h1  className="text-lg mb-4 font-semibold md:text-xl">
             One Rep Max Calculator
           </h1>
-          <p className="text-lg">
+          <p className="text-sm md:text-lg">
             Calculate your one-rep max (1RM) for any lift. Your one-rep max is
             the max weight you can lift for a single repetition for a given
             exercise.{" "}
           </p>
         </div>
-        <form
-          onSubmit={handleSubmit}
-          className="text-white flex flex-col items-center gap-4 mt-5"
-        >
-          <div className=" flex flex-col gap-4">
-            <div className="text-lg">Enter weight of the lift:</div>
-            <div className=" flex flex-row">
-              <input
-                type="number"
-                id="lift"
-
-                className=" bg-gray-800 p-2 rounded-sm text-white border"
-                onChange={(e) => setLift(e.target.value)}
-              />
-              <select
-                className=" bg-gray-800 text-white border rounded-slg"
-                onChange={(e) => setUnit(e.target.value)}
-              >
-                <option>kg</option>
-                <option>lb</option>
-              </select>
-            </div>
-            <div className=" flex flex-col gap-3 mr-3">
-              <div className="text-lg">Enter Repetitions:</div>
-              <input
-                type="number"
-                id="reps"
-
-                className=" bg-gray-800  p-2 rounded-sm text-white border w-1/4"
-                min="1"
-                max="10"
-                onChange={(e) => setReps(e.target.value)}
-              />
-            </div>
-            <button
-              type="submit"
-              className=" bg-red-900 border-none rounded-lg text-white py-2 px-3 hover:bg-red-800"
-            >
-              Calculate one rep max
-            </button>
+        <form onSubmit={handleSubmit} className=" my-7 ml-4">
+          <div class="group">
+            <input
+              className="body-input"
+              type="text"
+              required
+              onChange={(e) => setLift(e.target.value)}
+            />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label className="body-label">Weight of the lift</label>
           </div>
+
+          <div class="group">
+            <input
+              className="body-input"
+              type="text"
+              required
+              onChange={(e) => setReps(e.target.value)}
+            />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label className="body-label">Repetitions</label>
+          </div>
+          <button
+            type="submit"
+            className=" bg-blue-600 text-white rounded-lg hover:bg-blue-500 py-2 px-3"
+          >
+            Calculate
+          </button>
         </form>
+        {result ? (
+          <div className=" bg-zinc-600 p-5 text-white my-4 rounded-xl w-fit">
+            <h1 className="text-lg md:text-xl">
+              Your one-rep max is: {Math.round(result)} kg
+            </h1>
+          </div>
+        ) : null}
       </div>
-
-      {result ? (
-        <div className=" border p-5 text-white my-4 rounded-xl">
-          <h1 className="text-xl">
-            Your one-rep max is: {Math.round(result)}
-            <span className="ml-1">{unit}</span>
-          </h1>
-        </div>
-      ) : null}
-
-
+      <div className="mx-auto">
+        <h1 className="text-white text-sm mb-4 md:text-lg">Bellow is a table of other estimated rep maxes:</h1>
+        <table className=" text-gray-100 w-11/12 md:w-full">
+          <thead className=" bg-gray-700 text-sm md:text-xl">
+            <tr>
+              <th className="py-2 border-r border-gray-400 w-1/2">Rep max</th>
+              <th className="py-2">Weight</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">1RM</td>
+              <td className="py-2">{Math.round(result)} kg</td>
+            </tr>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">2RM</td>
+              <td className="py-2">{Math.round(result * 0.95)} kg</td>
+            </tr>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">3RM</td>
+              <td className="py-2">{Math.round(result * 0.92)} kg</td>
+            </tr>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">4RM</td>
+              <td className="py-2">{Math.round(result * 0.9)} kg</td>
+            </tr>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">5RM</td>
+              <td className="py-2">{Math.round(result * 0.88)} kg</td>
+            </tr>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">6RM</td>
+              <td className="py-2">{Math.round(result * 0.85)} kg</td>
+            </tr>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">7RM</td>
+              <td className="py-2">{Math.round(result * 0.83)} kg</td>
+            </tr>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">8RM</td>
+              <td className="py-2">{Math.round(result * 0.8)} kg</td>
+            </tr>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">9RM</td>
+              <td className="py-2">{Math.round(result * 0.77)} kg</td>
+            </tr>
+            <tr className=" bg-gray-800 text-gray-400 text-center border-b border-gray-500 text-sm md:text-lg">
+              <td className="py-2">10RM</td>
+              <td className="py-2">{Math.round(result * 0.75)} kg</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
