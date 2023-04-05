b4d1f231c1396df416fa442adb7d37392bff85a7
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-04-05 14:39:06 +0200

    design, api, input 2

--- src/components/Exercises/SearchedExercise.js
+++ src/components/Exercises/SearchedExercise.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchedExercise = ({ card }) => {
+
+
   return (
     <>
       {card.map((item) => {
         return (
-          <div style={{maxHeight: "390px"}} className="flex flex-col gap-4 text-black bg-slate-400" key={item.id}>
-            <img src={item.gifUrl} alt=""/>
-            <h1 className="text-lg">{item.name}</h1>
-            <h2 className="text-sm">Bodypart: {item.bodyPart}</h2>
+          <div style={{ maxHeight: "390px" }} className="flex flex-col gap-4 text-gray-900 bg-slate-400" key={item.id}>
+            <img style={{ maxWidth: "200px" }} src={item.gifUrl} alt="" />
+            <div className="flex flex-col gap-5  px-5">
+              <h1 className=" h-14 text-lg">{item.name}</h1>
+              <h2 className="text-sm font-bold mt-2">Bodypart: <span className="font-normal text-red-800">{item.bodyPart}</span></h2>
+            </div>
             <button className="text-gray-200 text-lg py-1 px-2 bg-green-700 hover:bg-green-600 mt-auto"><i class="bi bi-plus-square"></i></button>
           </div>
         );
@@ -18,3 +22,4 @@ const SearchedExercise = ({ card }) => {
 };
 
 export default SearchedExercise;
+
