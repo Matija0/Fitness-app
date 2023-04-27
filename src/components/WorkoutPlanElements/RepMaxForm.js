6769282ab8ebbb1a2740cb89b275ac8c295a07a4
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-04-27 13:29:56 +0200

    workout api

--- src/components/WorkoutPlanElements/RepMaxForm.js
+++ src/components/WorkoutPlanElements/RepMaxForm.js
@@ -12,42 +12,23 @@ import {
     setDoc,
 } from "firebase/firestore";
 import { useToast } from '@chakra-ui/react';
+import axios from 'axios';
 
 const RepMaxForm = () => {
 
-    const repmaxCollectionRef = collection(db, "repmaxdata")
-    const [show, setShow] = useState(true)
-    const [squat, setSquat] = useState("")
-    const [deadlift, setDeadlift] = useState("")
-    const [bench, setBench] = useState("")
-    const [ohp, setOhp] = useState("")
-    const [pullUp, setPullUp] = useState("")
-    const [incline, setIncline] = useState("")
-    const [rmdata, setRMData] = useState([])
+   
+    
+    const [time, setTime] = useState("")
+    const [muscle, setMuscle] = useState("")
+    const [location, setLocation] = useState("")
+    const [equipment, setEquipment] = useState("")
+    const [data, setData]=useState([])
+    
     const toast = useToast()
 
-    const addStats = async () => {
-        setShow(true)
-        await setDoc(doc(db, "repmaxdata", "mydata"), {
-            bench: bench,
-            deadlift: deadlift,
-            incline: incline,
-            ohp: ohp,
-            pullUp: pullUp,
-            squat: squat,
+    
 
-        });
-
-    }
-
-    useEffect(() => {
-        const getData = async () => {
-            const data = await getDocs(repmaxCollectionRef);
-            setRMData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-        };
-
-        getData();
-    }, []);
+   
 
     const notf = () => {
         toast({
@@ -59,120 +40,94 @@ const RepMaxForm = () => {
         })
     }
 
-    const docRef = doc(db, "repmaxdata", "mydata");
-
-    const data = {
-        bench: bench,
-        deadlift: deadlift,
-        incline: incline,
-        ohp: ohp,
-        pullUp: pullUp,
-        squat: squat
-
-    }
-
-    const updateStats = async (event) => {
-        event.preventDefault();
-        await updateDoc(docRef, data)
-        clear()
+    
+    const url = 'https://workout-planner1.p.rapidapi.com/?time=30&muscle=biceps&location=gym&equipment=dumbbells';
+    const options = {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/octet-stream',
+            'X-RapidAPI-Key': '12f7d92fcdmshb29dc70d54946e3p182334jsnd9a69db0fb7a',
+            'X-RapidAPI-Host': 'workout-planner1.p.rapidapi.com'
+        }
     };
-
-    const handleData = (event) => {
-        event.preventDefault();
-        addStats()
-        notf();
-        clear()
-    };
-
-    const clear = () => {
-        setSquat("")
-        setDeadlift("")
-        setBench("")
-        setOhp("")
-        setPullUp("")
-        setIncline("")
+    
+    const getData =  async() =>{
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            setData(result);
+        } catch (error) {
+            console.error(error);
+        }
     }
+    
 
+   useEffect(()=>{
+        getData()
+   },[]) 
+
+   
     return (
         <div className=" bg-gray-800 border border-gray-500 flex flex-col items-center gap-5 px-2 py-3 rounded-lg">
-            <h1 className="text-white text-xl">Enter your 1RMs:</h1>
-            <form onSubmit={rmdata ? (handleData) : (updateStats)} className="flex flex-col gap-3 items-center">
+            <h1 className="text-white text-xl">Enter data:</h1>
+            <form  className="flex flex-col gap-3 items-center">
                 <div class="group">
                     <input
                         className="body-input"
                         type="text"
                         required
-                        value={squat}
-                        onChange={(e) => setSquat(e.target.value)}
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}
                     />
                     <span className="highlight"></span>
                     <span className="bar"></span>
-                    <label className="body-label">Squat</label>
+                    <label className="body-label">Time</label>
                 </div>
                 <div class="group">
                     <input
                         className="body-input"
                         type="text"
                         required
-                        value={deadlift}
-                        onChange={(e) => setDeadlift(e.target.value)}
+                        value={muscle}
+                        onChange={(e) => setMuscle(e.target.value)}
                     />
                     <span className="highlight"></span>
                     <span className="bar"></span>
-                    <label className="body-label">Deadlift</label>
+                    <label className="body-label">Muscle</label>
                 </div>
                 <div class="group">
                     <input
                         className="body-input"
                         type="text"
                         required
-                        value={bench}
-                        onChange={(e) => setBench(e.target.value)}
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
                     />
                     <span className="highlight"></span>
                     <span className="bar"></span>
-                    <label className="body-label">Bench press</label>
+                    <label className="body-label">Location</label>
                 </div>
                 <div class="group">
                     <input
                         className="body-input"
                         type="text"
                         required
-                        value={ohp}
-                        onChange={(e) => setOhp(e.target.value)}
+                        value={equipment}
+                        onChange={(e) => setEquipment(e.target.value)}
                     />
                     <span className="highlight"></span>
                     <span className="bar"></span>
-                    <label className="body-label">OHP</label>
+                    <label className="body-label">Equipment</label>
                 </div>
-                <div class="group">
-                    <input
-                        className="body-input"
-                        type="text"
-                        required
-                        value={pullUp}
-                        onChange={(e) => setPullUp(e.target.value)}
-                    />
-                    <span className="highlight"></span>
-                    <span className="bar"></span>
-                    <label className="body-label">Weighted pull up</label>
-                </div>
-                <div class="group">
-                    <input
-                        className="body-input"
-                        type="text"
-                        required
-                        value={incline}
-                        onChange={(e) => setIncline(e.target.value)}
-                    />
-                    <span className="highlight"></span>
-                    <span className="bar"></span>
-                    <label className="body-label">Incline bench press</label>
-                </div>
-                {rmdata ? (<button type='submit' className='border-2 border-gray-300 text-gray-300 text-lg py-2 px-3 rounded-lg hover:bg-gray-300 hover:text-black'> <i class="bi bi-arrow-clockwise"></i></button>) : (<button type="submit" className="border-2 border-gray-300 text-gray-300 text-lg py-2 px-3 rounded-lg hover:bg-gray-300 hover:text-black"><i class="bi bi-box-arrow-in-right"></i></button>)}
+                
+                <button type='submit' className=' bg-sky-600 py-2 px-3 w-fit rounded-lg hover:bg-sky-500 text-gray-800'>Get workout</button>
             </form>
-
-            <p className="text-white text-sm">You can calculate your estimated one-rep maxes <Link to="/calculator"><span className="text-gray-500 hover:underline">here</span></Link> </p>
+        <div>
+            {data.map(()=>{
+                
+            })}
+        </div>
+           
         </div>
     )
 }
